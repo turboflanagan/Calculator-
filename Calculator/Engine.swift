667a2c70a8e740cf01a80a2fd97d7134bfc717e7
turboflanagan <turboflanagan@gmail.com>
2015-11-08 13:20:24 -0500

    First commit

---
+++ Calculator/Engine.swift
@@ -0,0 +1,174 @@
+//
+//  Engine.swift
+//  Calculator
+//
+//  Created by Joseph DeCarlo on 10/28/15.
+//  Copyright © 2015 DigitalCrafts. All rights reserved.
+//
+
+import Foundation
+
+enum CalculatorOperation {
+    
+    case Multiply
+    case Divide
+    case Add
+    case Subtract
+    case Percentage
+}
+
+protocol EngineDelegate {
+    func engineTotalDidChange(updatedTotal:Float)
+}
+
+class Engine {
+    
+    let displayZero = "0"
+    
+    var runningTotal : Float = 0.0 {
+        didSet {
+            print("runningTotal: \(runningTotal)")
+        }
+    }
+    var tempTotal : Float {
+        didSet {
+            print("tempTotal: \(tempTotal)")
+        }
+    }
+    var displayTotal : String
+    
+    init(delegate : EngineDelegate) {
+        self.displayTotal = self.displayZero
+        self.tempTotal = 0.0
+        self.delegate = delegate
+    }
+    
+    var delegate : EngineDelegate
+    var lastOperation : CalculatorOperation?
+    
+    func addDigit(digit : Int) {
+        
+        if self.displayTotal == self.displayZero {
+            
+            self.displayTotal = String(digit)
+        }
+        else {
+        
+            self.displayTotal += String(digit)
+        }
+        
+        self.delegate.engineTotalDidChange(Float(self.displayTotal)!);
+    }
+    
+    ///You will have to complete the other functions present
+    ///in the enumeration in the processOperand function
+    func addOperand(operand : CalculatorOperation) {
+        
+        if let last = self.lastOperation {
+            
+            processOperand(last)
+        }
+        else {
+            
+            processOperand(operand)
+        }
+        
+        self.lastOperation = operand
+        self.delegate.engineTotalDidChange(self.tempTotal)
+        self.displayTotal = self.displayZero
+    }
+    
+    ///You will have to complete the other functions present
+    ///in the enumeration
+    func equals() {
+
+        if let lastOp = self.lastOperation {
+
+            switch lastOp {
+            case .Add:
+                self.runningTotal = self.tempTotal + Float(self.displayTotal)!
+                break
+            case .Subtract:
+                let display = Float(self.displayTotal)!
+                if self.tempTotal > display {
+
+                    self.runningTotal = self.tempTotal - display
+                }
+                else {
+                    self.runningTotal = display - self.tempTotal
+                }
+
+                break
+            case .Multiply:
+                self.runningTotal = self.tempTotal * Float(self.displayTotal)!
+                break
+            case .Divide:
+                self.runningTotal = self.tempTotal / Float(self.displayTotal)!
+                break
+            case .Percentage:
+                break
+            }
+        }
+        
+        self.tempTotal = 0.0
+        self.displayTotal = self.displayZero
+        self.lastOperation = nil
+        self.delegate.engineTotalDidChange(self.runningTotal)
+
+    }
+    
+    func clearAll () {
+        
+        self.runningTotal = 0.0
+        self.tempTotal = 0.0
+        self.displayTotal = self.displayZero
+        self.lastOperation = nil
+        
+        
+        self.delegate.engineTotalDidChange(self.runningTotal)
+    }
+    
+    func clearDisplay() {
+        
+        self.displayTotal = self.displayZero
+    }
+    
+    //MARK:--- Private Functions ---
+    private func processOperand(operand: CalculatorOperation) {
+        
+        switch operand {
+        case .Add:
+            self.tempTotal = self.tempTotal + Float(self.displayTotal)!
+            break
+        case .Subtract:
+            let display = Float(self.displayTotal)!
+            if self.tempTotal > display {
+                
+                self.tempTotal = self.tempTotal - display
+            }
+            else {
+                self.tempTotal = display - self.tempTotal
+            }
+            
+            break
+        case .Multiply:
+            if self.tempTotal == 0 {
+                self.tempTotal = Float(self.displayTotal)!
+            }else {
+            self.tempTotal = self.tempTotal * Float(self.displayTotal)!
+            }
+            break
+        case .Divide:
+            if self.tempTotal == 0 {
+                self.tempTotal = Float(self.displayTotal)!
+            }else {
+                self.tempTotal = self.tempTotal / Float(self.displayTotal)!
+            }
+            break
+        case .Percentage:
+            break
+            
+        }
+        
+    }
+}
---
+++ Calculator/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Joseph DeCarlo on 10/28/15.
+//  Copyright © 2015 Digital Crafts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, EngineDelegate {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    var engine : Engine?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.engine = Engine(delegate: self)
+    }
+
+    
+    func engineTotalDidChange(updatedTotal: Float) {
+        self.displayLabel.text = String(updatedTotal)
+    }
+    
+    @IBAction func divideButtonTouched(sender: AnyObject) {
+        
+        self.engine?.addOperand(CalculatorOperation.Divide)
+    }
+    
+    
+    @IBAction func multiplyButtonTouched(sender: AnyObject) {
+        self.engine?.addOperand(CalculatorOperation.Multiply)
+    }
+    
+    @IBAction func subtractButtonTouched(sender: AnyObject) {
+        
+        self.engine?.addOperand(CalculatorOperation.Subtract)
+    }
+    
+    @IBAction func addButtonTouched(sender: AnyObject) {
+        self.engine?.addOperand(CalculatorOperation.Add)
+    }
+    
+    @IBAction func equalButtonTouched(sender: AnyObject) {
+        
+        self.engine?.equals()
+    }
+    
+    
+    @IBAction func percentButtonTouched(sender: AnyObject) {
+        self.engine?.
+    }
+    @IBAction func nineButtonTouched(sender: AnyObject) {
+        self.engine?.addDigit(9)
+    }
+    @IBAction func eightButtonTouched(sender: AnyObject) {
+        self.engine?.addDigit(8)
+    }
+    @IBAction func sevenButtonTouched(sender: AnyObject) {
+        self.engine?.addDigit(7)
+    }
+    
+    @IBAction func sixButtonTouched(sender: AnyObject) {
+        self.engine?.addDigit(6)
+    }
+    @IBAction func fiveButtonTouched(sender: AnyObject) {
+        self.engine?.addDigit(5)
+    }
+    @IBAction func fourButtonTouched(sender: AnyObject) {
+        self.engine?.addDigit(4)
+    }
+    @IBAction func threeButtonTouched(sender: AnyObject) {
+        self.engine?.addDigit(3)
+    }
+    
+    @IBAction func twoButtonTouched(sender: AnyObject) {
+        self.engine?.addDigit(2)
+    }
+
+    @IBAction func oneButtonTouched(sender: AnyObject) {
+        self.engine?.addDigit(1)
+    }
+    
+    @IBAction func zeroButtonTouched(sender: AnyObject) {        
+        self.engine?.addDigit(0)
+    }
+    
+    
+    @IBAction func clearButtonTouched(sender: AnyObject) {
+        
+        self.engine?.clearDisplay()
+    }
+    
+    @IBAction func allClearButtonTouched(sender: AnyObject) {
+        
+        self.engine?.clearAll()
+    }
+    
+}
+
